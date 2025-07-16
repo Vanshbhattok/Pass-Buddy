ed0ebda075bbd2ea59e2569d540fc91cd37daeca
Vansh bhatt <144117143+Vanshbhattok@users.noreply.github.com>
2025-07-16 23:28:14 +0530

    Uploaded the Frontend files

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,27 @@
+const TextAnimatedGradient = () => {
+  return (
+    <span className='inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-xl text-transparent'>
+      Sample Text
+    </span>
+  );
+};
+
+export default TextAnimatedGradient;
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         shine: {
+//           '0%': { 'background-position': '100%' },
+//           '100%': { 'background-position': '-100%' },
+//         },
+//       },
+//       animation: {
+//         shine: 'shine 5s linear infinite',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+export const animation = {
+    'text-gradient': 'text-gradient 1.5s linear infinite',
+};
+export const keyframes = {
+    'text-gradient': {
+        to: {
+            backgroundPosition: '200% center',
+        },
+    },
+};
